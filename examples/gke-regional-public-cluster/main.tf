9fe4b2fdba270153f129b6ef351dc9c70b2c8e2c
Riley Karson <rileykarson@google.com>
2019-01-24 08:23:44 -0800

    Clean up gke-regional-public-cluster example

--- examples/gke-regional-public-cluster/main.tf
+++ examples/gke-regional-public-cluster/main.tf
@@ -21,26 +21,25 @@ module "gke_cluster" {
   # source = "git::git@github.com:gruntwork-io/gke-cluster.git//modules/gke-cluster?ref=v0.0.1"
   source = "../../modules/gke-cluster"
 
-  project = "${var.project}"
-  region  = "${var.region}"
-  name    = "${var.cluster_name}"
+  name = "${var.cluster_name}"
 
-  network           = "${google_compute_network.main.name}"
-  subnetwork        = "${google_compute_subnetwork.main.name}"
-  ip_range_pods     = "${google_compute_subnetwork.main.secondary_ip_range.0.range_name}"
-  ip_range_services = "${google_compute_subnetwork.main.secondary_ip_range.1.range_name}"
-
-  #service_account   = "${var.compute_engine_service_account}"
+  project    = "${var.project}"
+  region     = "${var.region}"
+  network    = "${google_compute_network.main.name}"
+  subnetwork = "${google_compute_subnetwork.main.name}"
 }
 
 # Node Pool
 
 // Node Pool Resource
 resource "google_container_node_pool" "node_pool" {
-  name               = "main-pool"
-  project            = "${var.project}"
-  region             = "${var.region}"
-  cluster            = "${module.gke_cluster.name}"
+  provider = "google-beta"
+
+  name    = "main-pool"
+  project = "${var.project}"
+  region  = "${var.region}"
+  cluster = "${module.gke_cluster.name}"
+
   initial_node_count = "1"
 
   autoscaling {
@@ -61,23 +60,11 @@ resource "google_container_node_pool" "node_pool" {
       all-pools-example = "true"
     }
 
-    # for custom shutdown scripts etc
-    # metadata = ""
-
-
-    #[DEPRECATED] This field is in beta and will be removed from this provider. Use it in the the google-beta provider instead.
-    # See https://terraform.io/docs/providers/google/provider_versions.html for more details.
-    #taint = {
-    #  key    = "main-pool-example"
-    #  value  = "true"
-    #  effect = "PREFER_NO_SCHEDULE"
-    #}
-
     tags         = ["main-pool-example"]
     disk_size_gb = "30"
     disk_type    = "pd-standard"
-    #service_account = "${lookup(var.node_pools[count.index], "service_account", var.service_account)}"
-    preemptible = false
+    preemptible  = false
+
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
@@ -94,8 +81,7 @@ resource "google_container_node_pool" "node_pool" {
   }
 }
 
-# Network
-
+# TODO(rileykarson): Add proper VPC network config once we've made a VPC module
 resource "random_string" "suffix" {
   length  = 4
   special = false
@@ -103,23 +89,13 @@ resource "random_string" "suffix" {
 }
 
 resource "google_compute_network" "main" {
-  name                    = "cft-gke-test-${random_string.suffix.result}"
+  name                    = "${var.cluster_name}-network-${random_string.suffix.result}"
   auto_create_subnetworks = "false"
 }
 
 resource "google_compute_subnetwork" "main" {
-  name          = "cft-gke-test-${random_string.suffix.result}"
+  name          = "${var.cluster_name}-subnetwork-${random_string.suffix.result}"
   ip_cidr_range = "10.0.0.0/17"
   region        = "${var.region}"
   network       = "${google_compute_network.main.self_link}"
-
-  secondary_ip_range {
-    range_name    = "cft-gke-test-pods-${random_string.suffix.result}"
-    ip_cidr_range = "192.168.0.0/18"
-  }
-
-  secondary_ip_range {
-    range_name    = "cft-gke-test-services-${random_string.suffix.result}"
-    ip_cidr_range = "192.168.64.0/18"
-  }
 }
--- examples/gke-regional-public-cluster/outputs.tf
+++ examples/gke-regional-public-cluster/outputs.tf
@@ -1,5 +1,11 @@
 output "cluster_endpoint" {
-  #sensitive   = true
-  description = "Cluster endpoint"
+  description = "The IP address of the cluster master."
+  sensitive   = true
   value       = "${module.gke_cluster.endpoint}"
+}
+
+output "cluster_ca_certificate" {
+  description = "The public certificate that is the root of trust for the cluster. Encoded as base64."
+  sensitive   = true
+  value       = "${module.gke_cluster.cluster_ca_certificate}"
 }
