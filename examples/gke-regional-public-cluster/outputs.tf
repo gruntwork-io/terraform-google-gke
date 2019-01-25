3299135e95a8c8abdc4d1c5d3d86eef5ca87e44b
Rob Morgan <robbym@gmail.com>
2019-01-25 10:53:14 +0800

    shift cluster tests into stages add working circleci config

--- examples/gke-regional-public-cluster/outputs.tf
+++ examples/gke-regional-public-cluster/outputs.tf
@@ -4,8 +4,19 @@ output "cluster_endpoint" {
   value       = "${module.gke_cluster.endpoint}"
 }
 
+output "client_certificate" {
+  description = "Public certificate used by clients to authenticate to the cluster endpoint."
+  value       = "${module.gke_cluster.client_certificate}"
+}
+
+output "client_key" {
+  description = "Private key used by clients to authenticate to the cluster endpoint."
+  sensitive   = true
+  value       = "${module.gke_cluster.client_key}"
+}
+
 output "cluster_ca_certificate" {
-  description = "The public certificate that is the root of trust for the cluster. Encoded as base64."
+  description = "The public certificate that is the root of trust for the cluster."
   sensitive   = true
   value       = "${module.gke_cluster.cluster_ca_certificate}"
 }
--- test/gke_cluster_test.go
+++ test/gke_cluster_test.go
@@ -3,22 +3,85 @@ package test
 import (
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/gcp"
+	"github.com/gruntwork-io/terratest/modules/k8s"
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/shell"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGKECluster(t *testing.T) {
 	t.Parallel()
 
-	testFolder := test_structure.CopyTerraformFolderToTemp(t, "..", "examples")
-	terraformModulePath := filepath.Join(testFolder, "gke-regional-public-cluster")
+	// Uncomment any of the following to skip that section during the test
+	// os.Setenv("SKIP_create_test_copy_of_examples", "true")
+	// os.Setenv("SKIP_create_terratest_options", "true")
+	// os.Setenv("SKIP_terraform_apply", "true")
+	// os.Setenv("SKIP_configure_kubectl", "true")
+	// os.Setenv("SKIP_wait_for_workers", "true")
+	// os.Setenv("SKIP_cleanup", "true")
 
-	project := gcp.GetGoogleProjectIDFromEnvVar(t)
-	region := gcp.GetRandomRegion(t, project, nil, nil)
-	terratestOptions := createGKEClusterTerraformOptions(t, project, region, terraformModulePath)
-	defer terraform.Destroy(t, terratestOptions)
+	// Create a directory path that won't conflict
+	workingDir := filepath.Join(".", "stages", t.Name())
 
-	terraform.InitAndApply(t, terratestOptions)
+	test_structure.RunTestStage(t, "create_test_copy_of_examples", func() {
+		testFolder := test_structure.CopyTerraformFolderToTemp(t, "..", "examples")
+		logger.Logf(t, "path to test folder %s\n", testFolder)
+		terraformModulePath := filepath.Join(testFolder, "gke-regional-public-cluster")
+		test_structure.SaveString(t, workingDir, "gkeClusterTerraformModulePath", terraformModulePath)
+	})
+
+	test_structure.RunTestStage(t, "create_terratest_options", func() {
+		gkeClusterTerraformModulePath := test_structure.LoadString(t, workingDir, "gkeClusterTerraformModulePath")
+		uniqueID := random.UniqueId()
+		project := gcp.GetGoogleProjectIDFromEnvVar(t)
+		region := gcp.GetRandomRegion(t, project, nil, nil)
+		gkeClusterTerratestOptions := createGKEClusterTerraformOptions(t, uniqueID, project, region, gkeClusterTerraformModulePath)
+		test_structure.SaveString(t, workingDir, "uniqueID", uniqueID)
+		test_structure.SaveString(t, workingDir, "project", project)
+		test_structure.SaveString(t, workingDir, "region", region)
+		test_structure.SaveTerraformOptions(t, workingDir, gkeClusterTerratestOptions)
+	})
+
+	defer test_structure.RunTestStage(t, "cleanup", func() {
+		gkeClusterTerratestOptions := test_structure.LoadTerraformOptions(t, workingDir)
+		terraform.Destroy(t, gkeClusterTerratestOptions)
+	})
+
+	test_structure.RunTestStage(t, "terraform_apply", func() {
+		gkeClusterTerratestOptions := test_structure.LoadTerraformOptions(t, workingDir)
+		terraform.InitAndApply(t, gkeClusterTerratestOptions)
+	})
+
+	test_structure.RunTestStage(t, "configure_kubectl", func() {
+		gkeClusterTerratestOptions := test_structure.LoadTerraformOptions(t, workingDir)
+		project := test_structure.LoadString(t, workingDir, "project")
+		region := test_structure.LoadString(t, workingDir, "region")
+		clusterName := gkeClusterTerratestOptions.Vars["cluster_name"].(string)
+
+		// gcloud beta container clusters get-credentials example-cluster --region australia-southeast1 --project dev-sandbox-123456
+		cmd := shell.Command{
+			Command: "gcloud",
+			Args:    []string{"beta", "container", "clusters", "get-credentials", clusterName, "--region", region, "--project", project},
+		}
+
+		shell.RunCommand(t, cmd)
+	})
+
+	test_structure.RunTestStage(t, "wait_for_workers", func() {
+		verifyGkeNodesAreReady(t)
+	})
+}
+
+// Verify that all the nodes in the cluster reach the Ready state.
+func verifyGkeNodesAreReady(t *testing.T) {
+	kubeWaitUntilNumNodes(t, 3, 30, 10*time.Second)
+	k8s.WaitUntilAllNodesReady(t, 30, 10*time.Second)
+	readyNodes := k8s.GetReadyNodes(t)
+	assert.Equal(t, len(readyNodes), 3)
 }
