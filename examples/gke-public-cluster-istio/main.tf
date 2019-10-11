84fe9194b8d696bb618d7bb9396d7fb4193475be
Marcin Niemira <marcin.niemira@gmail.com>
2019-10-12 00:12:06 +1100

    fix CI

--- examples/gke-public-cluster-istio/main.tf
+++ examples/gke-public-cluster-istio/main.tf
@@ -55,6 +55,69 @@ module "gke_cluster" {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
+# CREATE A NODE POOL
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_container_node_pool" "node_pool" {
+  provider = google-beta
+
+  name     = "main-pool"
+  project  = var.project
+  location = var.location
+  cluster  = module.gke_cluster.name
+
+  initial_node_count = "1"
+
+  autoscaling {
+    min_node_count = "1"
+    max_node_count = "1"
+  }
+
+  management {
+    auto_repair  = "true"
+    auto_upgrade = "true"
+  }
+
+  node_config {
+    image_type   = "COS"
+    machine_type = "n1-standard-1"
+
+    labels = {
+      all-pools-example = "true"
+    }
+
+    # Add a public tag to the instances. See the network access tier table for full details:
+    # https://github.com/gruntwork-io/terraform-google-network/tree/master/modules/vpc-network#access-tier
+    tags = [
+      module.vpc_network.public,
+      "public-pool-example",
+    ]
+
+    disk_size_gb = "30"
+    disk_type    = "pd-standard"
+    preemptible  = false
+
+    service_account = module.gke_service_account.email
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  lifecycle {
+    ignore_changes = [initial_node_count]
+  }
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
+}
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
 # CREATE A CUSTOM SERVICE ACCOUNT TO USE WITH THE GKE CLUSTER
 # ---------------------------------------------------------------------------------------------------------------------
 
--- examples/gke-public-cluster-istio/variables.tf
+++ examples/gke-public-cluster-istio/variables.tf
@@ -26,13 +26,13 @@ variable "region" {
 variable "cluster_name" {
   description = "The name of the Kubernetes cluster."
   type        = string
-  default     = "example-cluster"
+  default     = "example-cluster-istio"
 }
 
 variable "cluster_service_account_name" {
   description = "The name of the custom service account used for the GKE cluster. This parameter is limited to a maximum of 28 characters."
   type        = string
-  default     = "example-cluster-sa"
+  default     = "example-cluster-sa-istio"
 }
 
 variable "cluster_service_account_description" {
@@ -46,7 +46,7 @@ variable "cluster_service_account_description" {
 variable "vpc_cidr_block" {
   description = "The IP address range of the VPC in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27."
   type        = string
-  default     = "10.6.0.0/16"
+  default     = "10.12.0.0/16"
 }
 
 # For the example, we recommend a /16 network for the secondary range. Note that when changing the size of the network,
@@ -54,7 +54,7 @@ variable "vpc_cidr_block" {
 variable "vpc_secondary_cidr_block" {
   description = "The IP address range of the VPC's secondary address range in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27."
   type        = string
-  default     = "10.7.0.0/16"
+  default     = "10.11.0.0/16"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
