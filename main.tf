f6771b87413b68571d64761a66ea15ec1d388d10
Petri Autero <petri.autero@gmail.com>
2020-04-09 12:44:04 +0300

    Update main.tf
    Co-Authored-By: Rob Morgan <robbym@gmail.com>

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY A GKE PRIVATE CLUSTER IN GOOGLE CLOUD PLATFORM
-# This is an example of how to use the gke-cluster module to deploy a public Kubernetes cluster in GCP with a
+# This is an example of how to use the gke-cluster module to deploy a private Kubernetes cluster in GCP.
 # Load Balancer in front of it.
 # ---------------------------------------------------------------------------------------------------------------------
 
