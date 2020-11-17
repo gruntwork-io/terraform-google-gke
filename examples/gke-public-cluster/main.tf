481fcadce3268fa91417ba3564571505a70109f4
Rob Morgan <robbym@gmail.com>
2020-11-17 11:04:07 +0800

    update vpc-network to tf13 compatible version

--- examples/gke-public-cluster/main.tf
+++ examples/gke-public-cluster/main.tf
@@ -146,7 +146,7 @@ resource "random_string" "suffix" {
 }
 
 module "vpc_network" {
-  source = "github.com/gruntwork-io/terraform-google-network.git//modules/vpc-network?ref=tf13"
+  source = "github.com/gruntwork-io/terraform-google-network.git//modules/vpc-network?ref=v0.6.0"
 
   name_prefix = "${var.cluster_name}-network-${random_string.suffix.result}"
   project     = var.project
